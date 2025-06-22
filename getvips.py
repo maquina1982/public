c963ef11ce4b0a7a99dd667a5d99ecedd6900c42
Minor Quiros <m1i9n8o2r@gmail.com>
2025-06-22 15:39:44 -0600

    test2

--- getvips.py
+++ getvips.py
@@ -1,13 +1,5 @@
-import requests
-import json
-import logging
-
-from urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-from getpass import getpass
-
 number1 = input ("Enter first number to multiply:")
-number2 = getpass ("Enter second number to multiply:")
+number2 = input ("Enter second number to multiply:")
 
 multiplication = number1 * number2
 
