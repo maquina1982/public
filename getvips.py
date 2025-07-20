7dea4a1bfaf9c5279f197fdcb92e315d1220e3c5
Minor Quiros <m1i9n8o2r@gmail.com>
2025-07-19 21:47:18 -0600

    test git in gui

--- getvips.py
+++ getvips.py
@@ -1,6 +1,6 @@
 number1 = int(input ("Enter first number to multiply:"))
 number2 = int(input ("Enter second number to multiply:"))
-
+#test
 multiplication = number1 * number2
 
 print(multiplication)
